29af7cfae5160bbd71fc2e70aa6c085ea80790ed
Gaurav <gaurav.kumar@ogilvy.com>
2024-12-02 13:40:38 +0530

    added js file

---
+++ blocks/quote/quote.js
@@ -0,0 +1,17 @@
+export default async function decorate(block) {
+
+    // Select the target <p> element where the iframe should be appended
+    const targetParagraph = document.querySelector('.quote-wrapper .quote.block div:nth-child(2) div p');
+    
+    // Create the iframe element
+    const iframe = document.createElement('iframe');
+    
+    // Set iframe attributes
+    iframe.src = "https://gentingsingapore.com/#!/en/investors/stock-information/share-quote-and-chart"; // Replace with your desired URL
+    iframe.width = "600";
+    iframe.height = "700";
+    iframe.style.border = "1px solid #ccc";
+    
+    // Append the iframe to the <p> element
+    targetParagraph.appendChild(iframe);
+}
