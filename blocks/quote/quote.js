95df29befc5cf44e880ab0fa6cc35ba96983f266
sudhir madaan <sudhir.madaan@verticurl.com>
2024-12-03 16:33:21 +0530

    CustomText component added

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,17 +1,18 @@
 export default async function decorate(block) {
 
-    // Select the target <p> element where the iframe should be appended
-    const targetParagraph = document.querySelector('.quote-wrapper .quote.block div:nth-child(2) div p');
+    console.log(block);
+    // // Select the target <p> element where the iframe should be appended
+    // const targetParagraph = document.querySelector('.quote-wrapper .quote.block div:nth-child(2) div p');
     
-    // Create the iframe element
-    const iframe = document.createElement('iframe');
+    // // Create the iframe element
+    // const iframe = document.createElement('iframe');
     
-    // Set iframe attributes
-    iframe.src = "https://gentingsingapore.com/#!/en/contact"; // Replace with your desired URL
-    iframe.width = "100%";
-    iframe.height = "1600";
-    iframe.style.border = "1px solid #ccc";
+    // // Set iframe attributes
+    // iframe.src = "https://gentingsingapore.com/#!/en/contact"; // Replace with your desired URL
+    // iframe.width = "100%";
+    // iframe.height = "1600";
+    // iframe.style.border = "1px solid #ccc";
     
-    // Append the iframe to the <p> element
-    targetParagraph.appendChild(iframe);
+    // // Append the iframe to the <p> element
+    // targetParagraph.appendChild(iframe);
 }
